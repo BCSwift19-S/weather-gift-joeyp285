6a5a7e87236b5eb1c013f22216a7428c925619e6
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-03-09 23:54:01 -0500

    thru 3.2

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Joseph on 3/9/19.
+//  Copyright © 2019 Joseph Parks. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
